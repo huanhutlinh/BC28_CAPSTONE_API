51b4d925a5a60d28b185c0912758a621d44cdc2a
Anh Le <renyk97@gmail.com>
2022-07-28 09:36:03 +0700

    Fix Relate product for detail page

--- controllers/detail.js
+++ controllers/detail.js
@@ -1,4 +1,4 @@
-function getProductById(myParam) {
+function getProductById(callback, myParam) {
   // Kết nối API (đường dẫn backend cung cấp)
   var promise = axios({
     url: "https://shop.cyberlearn.vn/api/Product/getbyid?id=" + myParam,
@@ -6,11 +6,17 @@ function getProductById(myParam) {
   });
 
   //Xử lý thành công
+  // promise.then(function (result) {
+  //   console.log("result", result.data.content);
+  //   //gọi hàm render Product detail
+  //   renderProductDetail(result.data.content);
+  // });
   promise.then(function (result) {
-    console.log("result", result.data.content);
-    //gọi hàm render Product detail
-    renderProductDetail(result.data.content);
-  });
+    let product = result.data.content;
+    let relateProduct = result.data.content.relatedProducts;
+    callback(product, relateProduct);
+
+  })
 
   //Xử lý thất bại
   promise.catch(function (err) {
@@ -33,6 +39,37 @@ function renderProductDetail(arrProductDetail) {
   document.querySelector(".price").innerHTML = arrProductDetail.price + "$";
 }
 
+function renderRelateProduct(obj, arrRelateProduct) {
+  let container = document.querySelector('.feature .products-list ');
+
+  let html = '';
+  let out = '';
+
+  for (const item of arrRelateProduct) {
+      html += `
+          <div class="col-xl-4 col-md-6 col-item">
+                      <div class="product-card">
+                          <div class="card-img">
+                              <img src="${item.image}" alt="">
+                          </div>
+                          <div class="card-body">
+                              <h3>${item.name}</h3>
+                              <p>${item.shortDescription}</p>
+                              <div class="d-flex price-wrap align-items-center">
+                                  <a href="../views/detail.html?id=${item.id}" class="btn-primary-cus">
+                                      <span>Buy Now</span>
+                                  </a>
+                                  <span class="price">$${item.price}</span>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+          `
+  }
+  out = '<div class="row">' + html + '</div>';
+  container.innerHTML = out;
+}
+
 window.onload = function () {
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get("id");
@@ -40,6 +77,6 @@ window.onload = function () {
 
   //call api load lên giao diện
 
-  getProductById(myParam ? myParam : 1);
-  getProductAPI(loadInitialItems);
+  getProductById(renderProductDetail, myParam ? myParam : 1);
+  getProductById(renderRelateProduct,myParam ? myParam : 1);
 };
