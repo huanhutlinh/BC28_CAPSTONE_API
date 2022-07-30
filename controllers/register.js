d06c78a57c2851c32df58ce643d49b0cf857eb49
Linh Hua <huanhutlinh128@gmail.com>
2022-07-31 00:28:09 +0700

    Use POST method to push UserInfo

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,19 @@
+function processForm(e) {
+  if (e.preventDefault) e.preventDefault();
+
+  /* do what you want with the form */
+
+  // You must return false to prevent the default form behavior
+  return false;
+}
+
+var form = document.getElementById("register-form");
+if (form.attachEvent) {
+  form.attachEvent("submit", processForm);
+} else {
+  form.addEventListener("submit", processForm);
+}
+
 document.querySelector("#btnRegister").onclick = function () {
   console.log(123);
   //Lấy thông tin User đúng format backend qui định
@@ -5,7 +21,7 @@ document.querySelector("#btnRegister").onclick = function () {
   user.email = document.querySelector("#email").value;
   user.password = document.querySelector("#password").value;
   user.name = document.querySelector("#name").value;
-  user.gender = document.querySelector("#gender").value;
+  user.gender = document.querySelector('input[name="genderS"]:checked').value;
   user.phone = document.querySelector("#phone").value;
 
   console.log("user", user);
